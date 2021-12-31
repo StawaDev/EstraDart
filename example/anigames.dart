a9965aec62762bf92a6c165d421bb5ae75309e0a
unknown <damodaraswarupa@gmail.com>
2021-12-31 14:06:01 +0800

    Updated Example AniGames.dart

--- example/anigames.dart
+++ example/anigames.dart
@@ -6,12 +6,13 @@ import 'package:estradart/estradart.dart';
 void main() async {
   firstExample();
   secondExample();
+  thirdExample();
 }
 
 // First Example
 void firstExample() async {
-  print(await EstraAniGames.truth());
-  print(await EstraAniGames.dare());
+  print("Truth Challenge: ${await EstraAniGames.truth()}");
+  print("Dare challenge: ${await EstraAniGames.dare()}");
 }
 
 // Second Examples
@@ -19,5 +20,12 @@ void secondExample() async {
   var GetWaifu = EstraAniGames.InputLink = await EstraAniGames.waifu();
   print("Waifu Picture: $GetWaifu");
   print("Waifu Name: ${await EstraAniGames.waifu_name()}");
+  // Just an reminder, you can't use two waifu_name function in once or it will return the same name!
+}
+
+void thirdExample() async {
+  var GetHusbando = EstraAniGames.InputLink = await EstraAniGames.husbando();
+  print("Husbando Picture: $GetHusbando");
+  print("Husbando Name: ${await EstraAniGames.husbando_name()}");
   // Just an reminder, you can't use two waifu_name function in once or it will return the same name!
 }
