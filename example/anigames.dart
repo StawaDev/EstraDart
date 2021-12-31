d7deb9a30ba9c0a22bc17e7d0c73ddc07177e1a6
unknown <damodaraswarupa@gmail.com>
2021-12-31 13:26:02 +0800

    Updated AniGames Example

--- example/anigames.dart
+++ example/anigames.dart
@@ -2,8 +2,21 @@ library Estra;
 
 import 'package:estradart/estradart.dart';
 
+// Trigger
 void main() async {
+  firstExample();
+  secondExample();
+}
+
+// First Example
+void firstExample() async {
   print(await EstraAniGames.truth());
-  print(await EstraAniGames.waifu());
-  // More EndPoint Use EstraHelp Function!
+  print(await EstraAniGames.dare());
+}
+
+// Second Examples
+void secondExample() async {
+  var GetWaifu = EstraAniGames.InputLink = await EstraAniGames.waifu();
+  print("Waifu Picture: $GetWaifu");
+  print("Waifu Name: ${await EstraAniGames.waifu_name()}");
 }
