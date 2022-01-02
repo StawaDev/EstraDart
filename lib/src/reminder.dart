57f7f89f73e646a2fed2f1b0d1a3d2eb89df7e5e
unknown <damodaraswarupa@gmail.com>
2022-01-02 14:46:19 +0800

    Added Description

--- lib/src/reminder.dart
+++ lib/src/reminder.dart
@@ -1,6 +1,20 @@
 import 'package:estradart/http.dart';
 
+/// ### EstraReminderBase
+/// This Class Will Be Remidning Users Something About EstraDart, Like Reminding Update of EstraDart
+/// ### Examples
+/// ```
+/// library Estra;
+/// import 'package:estradart/estradart.dart';
+///
+/// void main() {
+///   EstraReminder.UpdateReminder();
+/// }
+///```
+/// ### Important Note
+/// As always, do NOT use this class function with print method, because it will return null as well.
 class EstraReminderBase {
+  /// Reminding Users about Updating EstraDart to Latest Version.
   UpdateReminder() async {
     var UtD_Ver = await HttpManager.Version();
     var C_Ver = "0.1.5";
