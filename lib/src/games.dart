33639b4b903a2eddd9a6d3b6363df33c7f0254b5
unknown <damodaraswarupa@gmail.com>
2022-02-05 11:44:18 +0800

    Added New Functions Shipper

--- lib/src/games.dart
+++ lib/src/games.dart
@@ -1,6 +1,11 @@
 import 'package:estradart/base.dart';
 import 'package:estradart/http.dart';
 
+const size = {
+  "width": 1920,
+  "height": 1080,
+};
+
 /// ### Games Class
 /// Games Class Have Their Own Endpoint Functions, Once You Put The Correct Endpoint, It Will Return Normally,
 /// But Once You Put The Invalid Endpoint It Will Gives You an Error Messages.
@@ -8,9 +13,13 @@ import 'package:estradart/http.dart';
 /// ### Examples
 /// This is an simple example of an Games endpoints functions.
 /// ```dart
+/// library Estra;
+/// import 'package:estradart/estradart.dart';
+/// var Client = new EstraClient();
+///
 /// void main() async {
-/// print(await EstraGames.truth());
-///  print(await EstraGames.dare());
+///   print(await Client.Games.truth(2));
+///   print(await Client.Games.dare());
 ///}
 ///```
 class TypeGames {
@@ -18,10 +27,13 @@ class TypeGames {
 
   /// Return Truth as Text
   /// ### Generate Examples
-  /// In here you can actually generate more than one GIF! Max 10. And Of course, it will return a list type.
+  /// In here you can actually generate more than one Text! Max 10. And Of course, it will return a list type.
   /// ```
+  /// library Estra;
+  /// import 'package:estradart/estradart.dart';
+  /// var Client = new EstraClient();
   /// void main() async {
-  ///   print(await EstraGames.truth(5));
+  ///   print(await Client.Games.truth(5));
   /// }
   /// ```
   truth([int generate = 0]) async {
@@ -34,10 +46,13 @@ class TypeGames {
 
   /// Return Dare as Text
   /// ### Generate Examples
-  /// In here you can actually generate more than one GIF! Max 10. And Of course, it will return a list type.
+  /// In here you can actually generate more than one Text! Max 10. And Of course, it will return a list type.
   /// ```
+  /// library Estra;
+  /// import 'package:estradart/estradart.dart';
+  /// var Client = new EstraClient();
   /// void main() async {
-  ///   print(await EstraGames.dare(5));
+  ///   print(await Client.Games.dare(5));
   /// }
   /// ```
   dare([int generate = 0]) async {
@@ -46,5 +61,56 @@ class TypeGames {
       return produce(Category, "dare", "text", generate);
     }
     return await HttpManager.TypeText();
+  }
+
+  /// Setup Data For Shipper Function.
+  /// ### Simple Setup Example
+  /// ```
+  /// var Client = TypeGames();
+  /// const size = {
+  ///   "width": "1280",
+  ///   "height": "720",
+  ///};
+  /// void main() {
+  ///   var data = await Client.Games.setupShipper("Player", "Player_Image", "Player2", "Player2_Image", "Background", size);
+  /// }
+  ///```
+  setupShipper(
+      [String player = "None",
+      String player_image = "None",
+      String player2 = "None",
+      String player2_image = "None",
+      String background = "None",
+      Map background_size = size]) async {
+    var width = background_size["width"];
+    var height = background_size["height"];
+    var url =
+        "$BASE_URL/games/shipper/image/?player=$player&player_image=$player_image&player2=$player2&player2_image=$player2_image&background=$background&background_size=${width}x$height";
+    return url;
+  }
+
+  /// Return Shipper as Image
+  /// ### Shipper Examples
+  /// ```
+  /// void main() async {
+  ///   print(await Client.Games.shipper(await data, false, false, true));
+  /// }
+  /// ```
+  shipper(
+      [url = "None",
+      bool openImageOnly = false,
+      bool downloadImageOnly = false,
+      bool downloadOpenImage = false]) async {
+    if (openImageOnly) {
+      return await openImageTemp(url);
+    }
+    if (downloadImageOnly) {
+      return await downloadImage(url, "shipper");
+    }
+    if (downloadOpenImage) {
+      await downloadImage(url, "shipper");
+      await openImageDirectory("shipper");
+      return "Downloaded and Opened Shipper File!";
+    }
   }
 }
